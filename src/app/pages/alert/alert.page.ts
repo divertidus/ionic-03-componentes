9f29f4f2fa62048dc750d8e652d274a650c31913
nacho <nacho@teconsite.com>
2024-09-24 16:33:53 +0200

    Alerts, antes de entender como se llama a cada alerta por separado, fin dia

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,20 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonButton, IonAlert } from '@ionic/angular/standalone';
 import { HeaderComponent } from 'src/app/components/header/header.component';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.page.html',
   styleUrls: ['./alert.page.scss'],
   standalone: true,
-  imports: [HeaderComponent,IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonAlert, IonButton, HeaderComponent, IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  // Configuración de la primera alerta 
+  alertBackdropDismiss = true;
+  alertHeader = 'Titulo del alert';
+  alertSubHeader = 'Subtítulo del alert';
+  alertMessage = 'Mensaje a mostrar, soy un alert';
+  alertButtons = [
+    {
+      text: 'OK',
+      role: 'confirm',
+      handler: () => {
+        console.log('Confirmado');
+      }
+    },
+    {
+      text: 'Cancelar',
+      role: 'cancel',
+      handler: () => {
+        console.log('Cancelado');
+      }
+    }
+  ];
 
+
+  //Esto es para la segunda alerta
+  constructor(private alertController: AlertController) { }
+  // Método para la segunda alerta programática
+  async presentSecondAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alerta programada',
+      subHeader: 'Segunda alerta',
+      message: 'Este es otro tipo de alerta programada',
+      buttons: [
+        {
+          text: 'Aceptar',
+          handler: () => {
+            console.log('Alerta programada aceptada');
+          }
+        },
+        {
+          text: 'Cerrar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Alerta programada cerrada');
+          }
+        }
+      ]
+    });
+
+    // Muestra la alerta programada
+    await alert.present();
+  }
   ngOnInit() {
   }
 
