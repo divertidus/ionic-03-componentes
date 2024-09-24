83fb2cf8098b6da2ab1501d2a65e3f4ff4c69e74
nacho <nacho@teconsite.com>
2024-09-24 13:07:28 +0200

    Lista desde componente, iconos, navega, color, detail entre SO

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,17 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonTabButton, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonTabButton, IonButton, IonList, IonLabel, IonItem, IonIcon } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
+
+/*Defino una interfaz con lo que tendrá que tener cada tipo Componente que cree a posteriori*/
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
   standalone: true,
-  imports: [IonButton, IonTabButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink]
+  imports: [IonIcon, IonItem, IonLabel, IonList, IonList, IonButton, IonTabButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink]
 })
 export class InicioPage implements OnInit {
+
+  /*Creo componentes indicando que será de tipo Componente para que tenga que implementar lo que pusimos en la interfaz superior.*/
+  componentes: Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'megaphone-outline',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+  ]
 
   constructor() { }
 
